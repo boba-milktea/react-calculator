3ba8db277ad22fb85f2313326691b0eb62613b96
boba-milktea <catherine.idv@gmail.com>
2025-05-21 16:25:48 +0200

    add the header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import Calculator from './components/Calculator';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <main className="grid place-items-center h-screen text-center">
-      <section className="flex flex-col items-center">
-        <h1 className="text-4xl text-pink-700 font-mono font-bold uppercase">
-          🔢 Simple React Calculator
-        </h1>
-        <p className="text-xl font-sans text-lime-800 p-4">
-          A simple calculator built with React, TypeScript, and Tailwind CSS.
-        </p>
-      </section>
-      <Calculator />
-    </main>
+    <>
+      <Header />
+      <main className="text-center flex flex-col items-center">
+        <Calculator />
+      </main>
+    </>
   );
 };
 
